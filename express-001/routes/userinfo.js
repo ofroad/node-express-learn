0a9aca0e40c10f4e2da2f0060bac61edc214495d
ofroad <wh724724@qq.com>
2018-05-21 17:06:17 +0800

    增加cors

--- express-001/routes/userinfo.js
+++ express-001/routes/userinfo.js
@@ -2,11 +2,26 @@ var express = require('express');
 var router = express.Router();
 var URL = require('url');
 var userapi = require('./userapi');
+
+//设置cors
+router.all('*',function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+	res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+	res.header("Cache-Control", "no-store");
+    next();
+});
+
 /* GET users listing. */
 router.get('/getUserInfo', function(req, res, next) {
     var user = new userapi();
     var params = URL.parse(req.url, true).query;
-	console.log(typeof req.query)
+	console.log(typeof req.query);
+	console.log("req.hostname==",req.hostname);
+	console.log("req.ip==",req.ip);
+	console.log("req.originalUrl==",req.originalUrl);
   console.log(params)
   
   //参数判断
